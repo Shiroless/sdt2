47c5b490c4fa485a8ae51db63e5d46dc409e6e7f
Shiro <36568774+Shiroless@users.noreply.github.com>
2024-11-28 20:33:10 +0100

    another change

--- src/sdt2/Main.java
+++ src/sdt2/Main.java
@@ -20,26 +20,15 @@ public class Main {
 
         Arrays.sort(f1drivers);
         System.out.println("List of drivers");
+        int i = 0;
         for (String driver : f1drivers) {
+        	System.out.println("Spot " + i);
+        	i++;
             System.out.println(driver);
         }
         
         Random rand = new Random();
-        int[] grill = new int[f1drivers.length];
-        
-        for (int i = 0; i < grill.length; i++) {
-            int randpos;
-            do randpos = rand.nextInt(f1drivers.length) + 1; while (existpos(grill, randpos));
-            grill[i] = randpos;
-        }
-
-        System.out.println("final grill:");
-        for (int i = 0; i < f1drivers.length; i++)
-            System.out.println("Spot " + grill[i] + ": " + f1drivers[i]);
-    }
-
-    public static boolean existpos(int[] grill, int pos) {
-        for (int p : grill) if (p == pos) return true;
-        return false;
+        int x = rand.nextInt(f1drivers.length);
+        System.out.println("Driver selected: " + f1drivers[x]);
     }
 }
